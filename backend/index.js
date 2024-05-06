3d0fedf5e94ffec978683f9c45b71ea366a27d9a
Ayushi Garg <ayushigarg2709@gmail.com>
2024-05-06 23:07:03 +0530

    fixing links and server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,22 @@
-console.log("yeah running");
+// importing packages
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import path from "path";
+
+// files
+import connectDB from "./config/db.js";
+//configuration
+dotenv.config();
+connectDB();
+const app = express(); //instance of express js
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cookieParser());
+const PORT = process.env.PORT || 3000;
+//Routes
+  app.listen(PORT, () => 
+     console.log(`API server running on port ${PORT}`));
+
+
